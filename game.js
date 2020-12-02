8d4cf4f39db39ef3a56cd338924f7b962819bb9e
Mateusz Nowak <nowak.mat1994@gmail.com>
2020-12-02 18:20:28 +0100

    add class Game

--- game.js
+++ game.js
@@ -1,1 +1,57 @@
-// link do gry: https://websamuraj.pl/examples/js/gra/ 
+class Game {
+    constructor(start) {
+        this.stats = new Statistics();
+        this.wallet = new Wallet(start);
+        document.getElementById('start').addEventListener('click', this.startGame.bind(this));
+
+        this.spanWallet = document.querySelector('.panel span.wallet');
+        this.boards = [...document.querySelectorAll('div.color')];
+        this.inputBid = document.getElementById('bid');
+        this.spanResult = document.querySelector('.score span.result');
+        this.spanGames = document.querySelector('.score span.number');
+        this.spanWins = document.querySelector('.score span.win');
+        this.spanLosses = document.querySelector('.score span.loss');
+
+        this.render()
+
+    }
+
+    render(colors = ['grey', 'grey', 'grey'], money = this.wallet.getWalletValue(), result = '', stats = [0, 0, 0], bid = 0, wonMoney = 0) {
+
+        this.boards.forEach((board, index) => {
+            board.style.backgroundColor = colors[index];
+        })
+
+        this.spanWallet.textContent = money;
+        if (result) {
+            result = `Wygrałęś ${wonMoney}`;
+        } else if (!result && result !== "") {
+            result = `Przegrałeś ${bid}`;
+        }
+        this.spanResult.textContent = result;
+        this.spanGames.textContent = stats[0];
+        this.spanWins.textContent = stats[1];
+        this.spanLosses.textContent = stats[2];
+    }
+
+    startGame() {
+        if (this.inputBid.value < 1) return alert('za mała kwota');
+        const bid = Math.floor(this.inputBid.value);
+
+        if (!this.wallet.checkCanPlay(bid)) {
+            return alert('masz za mało środków, lub podełeś nieprawidłową wartosć');
+        }
+
+        this.wallet.changeWallet(bid, '-');
+        this.draw = new Draw();
+        const colors = this.draw.getDrawResult();
+        const win = Result.checkWinner(colors);
+        const wonMoney = Result.moneyWinInGame(win, bid);
+        this.wallet.changeWallet(wonMoney);
+        this.stats.addGameToStatistics(win, bid);
+
+        this.render(colors, this.wallet.getWalletValue(), win, this.stats.showGameStatistics(), bid, wonMoney)
+
+        this.inputBid.value = "";
+    }
+}
